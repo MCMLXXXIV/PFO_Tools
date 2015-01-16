94b9bd6ac1883b5f01ba1375a963c90ccb1187b9
MCMLXXXIV <dbl_jr@yahoo.com>
2015-01-15 19:21:28 -0800

    now can pass entity and rank as parameters

--- demo_site/Scripts/utils.js
+++ demo_site/Scripts/utils.js
@@ -28,3 +28,28 @@ function PrettyPrintDuration(dur) {
 
     return val + " " + unit;
 }
+
+function getUrlParameters(parameter, staticURL, decode){
+   /*
+    Function: getUrlParameters
+    Description: Get the value of URL parameters either from 
+                 current URL or static URL
+    Author: Tirumal
+    URL: www.code-tricks.com
+   */
+    var currLocation = (staticURL.length)? staticURL : window.location.search,
+    parArr = currLocation.split("?")[1].split("&"),
+    returnBool = true;
+   
+    for(var i = 0; i < parArr.length; i++){
+        parr = parArr[i].split("=");
+        if(parr[0] == parameter){
+            return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+            returnBool = true;
+        }else{
+            returnBool = false;            
+        }
+    }
+   
+    if(!returnBool) return false;  
+}
